0c052273eb19f4ab32da1f4b074ff000168fed6a
Kareemts <9020279471kareem@gmail.com>
2023-06-02 21:56:17 +0530

    deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const createError = require("http-errors");
 const morgan = require("morgan");
 const cors = require("cors");
-const path = require('path');
+const path = require("path");
 require("dotenv").config();
 const { connection } = require("./src/config/db.config");
 const app = express();
@@ -20,15 +20,6 @@ app.use(morgan("dev"));
 // });
 
 app.use("/api/admin", require("./src/routes/adminRouter"));
-
-__dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
 
 app.use((req, res, next) => {
   next(createError.NotFound());
